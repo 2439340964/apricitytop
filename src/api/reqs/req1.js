245dcb1282ab0d553cf2fdeda0ea2f5cce2fef39
zeming <8741248+zeminga@user.noreply.gitee.com>
2022-11-06 10:01:48 +0800

    配置nginx代理

--- src/api/reqs/req1.js
+++ src/api/reqs/req1.js
@@ -2,8 +2,7 @@ import HttpReq from "@/lib/axios";
 
 const req1 = new HttpReq();
 // 用这种方式可以独立的配置不同的ip  baseURL
-req1.config.baseURL = "http://124.221.2.47:8031";
-console.log(req1);
+req1.config.baseURL = "https://apricityzm.top/api";
 
 
 export default req1;
